edb590010c85ee0c741810cd4da94d8c6d846b20
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-01-19 00:19:06 +0800

    feat: mandelbrot set implemented in Scheme

---
+++ mandelbrot.scm
@@ -0,0 +1,35 @@
+(define real-min -2.5)
+(define real-max +1.5)
+(define imag-min -2.0)
+(define imag-max +2.0)
+
+(define iter-count 40)
+(define escape-radius 4.0)
+
+(define width 1024)
+(define scale (/ width (- real-max real-min)))
+(define height (round->exact (* scale (- imag-max imag-min))))
+
+(define (complex real imag) (make-rectangular real imag))
+
+(define (mandelbrot x y)
+  (define (*mandelbrot z0 z rem)
+    (if (or (= rem 0) (>= (magnitude z) escape-radius))
+      rem
+      (*mandelbrot z0 (+ (* z z) z0) (- rem 1))))
+  (let ((val (complex (+ (/ x scale) real-min)
+                      (+ (/ y scale) imag-min))))
+    (round (- 255 (* 255 (/ (- iter-count (*mandelbrot val 0 iter-count)) iter-count))))))
+
+(define (plot file)
+  (with-output-to-file file
+    (lambda () (begin
+      (display "P2") (newline)
+      (display width) (newline)
+      (display height) (newline)
+      (display 255) (newline)
+      (do ((y 0 (+ y 1))) ((>= y height))
+        (do ((x 0 (+ x 1))) ((>= x width))
+          (begin (display (mandelbrot x y)) (newline))))))))
+
+(plot "mandelbrot.pbm")
