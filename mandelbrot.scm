c2486b246f0b2253e1b35511a18abb46637f3d3e
chenzhuoyu <chenzhuoyu@bytedance.com>
2022-01-19 16:22:18 +0800

    feat: use proper output port

--- mandelbrot.scm
+++ mandelbrot.scm
@@ -3,6 +3,7 @@
 (define imag-min -2.0)
 (define imag-max +2.0)
 
+(define max-color 255)
 (define iter-count 40)
 (define escape-radius 4.0)
 
@@ -10,26 +11,32 @@
 (define scale (/ width (- real-max real-min)))
 (define height (round->exact (* scale (- imag-max imag-min))))
 
-(define (complex real imag) (make-rectangular real imag))
+(define (complex real imag)
+  (make-rectangular real imag))
 
 (define (mandelbrot x y)
   (define (*mandelbrot z0 z rem)
     (if (or (= rem 0) (>= (magnitude z) escape-radius))
-        rem
+        (round (* (/ rem iter-count) max-color))
         (*mandelbrot z0 (+ (* z z) z0) (- rem 1))))
   (let ((val (complex (+ (/ x scale) real-min)
                       (+ (/ y scale) imag-min))))
-    (round (- 255 (* 255 (/ (- iter-count (*mandelbrot val 0 iter-count)) iter-count))))))
+    (*mandelbrot val 0 iter-count)))
 
 (define (plot file)
-  (with-output-to-file file
-    (lambda () (begin
-      (display "P2") (newline)
-      (display width) (newline)
-      (display height) (newline)
-      (display 255) (newline)
-      (do ((y 0 (+ y 1))) ((>= y height))
-        (do ((x 0 (+ x 1))) ((>= x width))
-          (begin (display (mandelbrot x y)) (newline))))))))
+  (call-with-output-file file
+    (lambda (fp)
+      (begin
+        (display "P2" fp) (newline fp)
+        (display width fp) (newline fp)
+        (display height fp) (newline fp)
+        (display max-color fp) (newline fp)
+        (do ((y 0 (+ y 1))) ((>= y height))
+          (begin
+            (do ((x 0 (+ x 1))) ((>= x width))
+              (begin
+                (display (mandelbrot x y) fp)
+                (display #\Space fp)))
+            (newline fp)))))))
 
-(plot "mandelbrot.pbm")
+(plot "mandelbrot.pgm")
